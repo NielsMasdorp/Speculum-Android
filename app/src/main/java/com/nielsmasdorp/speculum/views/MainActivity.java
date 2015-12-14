435fc49f712e34f1606f117c03d8b4d1ec1c73d3
Gijs de Wit <gijs.de.wit94@gmail.com>
2015-12-15 00:40:56 +0100

    Immersive mode (hides system UI); Location textcolor white

--- app/src/main/java/com/nielsmasdorp/speculum/views/MainActivity.java
+++ app/src/main/java/com/nielsmasdorp/speculum/views/MainActivity.java
@@ -45,6 +45,8 @@ public class MainActivity extends AppCompatActivity implements IMainView {
 
     MainPresenter mMainPresenter;
 
+    private View mDecorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,11 +59,8 @@ public class MainActivity extends AppCompatActivity implements IMainView {
         mMainPresenter = new MainPresenter(this);
         mMainPresenter.loadWeather(location);
 
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-
+        mDecorView = getWindow().getDecorView();
+        hideSystemUI();
     }
 
     @Override
@@ -80,6 +79,19 @@ public class MainActivity extends AppCompatActivity implements IMainView {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     @Override
