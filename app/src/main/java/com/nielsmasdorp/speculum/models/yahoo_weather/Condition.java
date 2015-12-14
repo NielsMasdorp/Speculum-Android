80fd7bdd4441480ee10d8ae230128e91c04aab14
NielsMasdorp <nielsmasdorp@gmail.com>
2015-12-15 00:53:27 +0100

    calendar event

--- app/src/main/java/com/nielsmasdorp/speculum/models/yahoo_weather/Condition.java
+++ app/src/main/java/com/nielsmasdorp/speculum/models/yahoo_weather/Condition.java
@@ -1,8 +1,4 @@
-
 package com.nielsmasdorp.speculum.models.yahoo_weather;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Condition {
 
@@ -10,14 +6,4 @@ public class Condition {
     public String date;
     public String temp;
     public String text;
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
 }
--- app/src/main/java/com/nielsmasdorp/speculum/models/yahoo_weather/Units.java
+++ app/src/main/java/com/nielsmasdorp/speculum/models/yahoo_weather/Units.java
@@ -1,8 +1,4 @@
-
 package com.nielsmasdorp.speculum.models.yahoo_weather;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Units {
 
@@ -10,14 +6,4 @@ public class Units {
     public String pressure;
     public String speed;
     public String temperature;
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
 }
