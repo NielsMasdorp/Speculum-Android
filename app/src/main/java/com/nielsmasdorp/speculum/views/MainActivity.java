960fcb9beedf5bd365c48a65de969bcc78a007e3
NielsMasdorp <nielsmasdorp@gmail.com>
2015-12-15 10:38:18 +0100

    removed menu

--- app/src/main/java/com/nielsmasdorp/speculum/views/MainActivity.java
+++ app/src/main/java/com/nielsmasdorp/speculum/views/MainActivity.java
@@ -92,24 +92,6 @@ public class MainActivity extends AppCompatActivity implements IMainView {
                 });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void hideSystemUI() {
         // Set the IMMERSIVE flag.
         // Set the content to appear under the system bars so that the content
