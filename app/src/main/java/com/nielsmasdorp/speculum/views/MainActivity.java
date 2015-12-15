ea72f332e18d82daa10162b02a0027c8e8bb599f
Gijs de Wit <gijs.de.wit94@gmail.com>
2015-12-15 01:00:42 +0100

    Fixed immersive mode

--- app/src/main/java/com/nielsmasdorp/speculum/views/MainActivity.java
+++ app/src/main/java/com/nielsmasdorp/speculum/views/MainActivity.java
@@ -61,6 +61,27 @@ public class MainActivity extends AppCompatActivity implements IMainView {
 
         mDecorView = getWindow().getDecorView();
         hideSystemUI();
+
+        mDecorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        // Note that system bars will only be "visible" if none of the
+                        // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            // TODO: The system bars are visible. Make any desired
+                            // adjustments to your UI, such as showing the action bar or
+                            // other navigational controls.
+
+                            hideSystemUI();
+
+                        } else {
+                            // TODO: The system bars are NOT visible. Make any desired
+                            // adjustments to your UI, such as hiding the action bar or
+                            // other navigational controls.
+                        }
+                    }
+                });
     }
 
     @Override
