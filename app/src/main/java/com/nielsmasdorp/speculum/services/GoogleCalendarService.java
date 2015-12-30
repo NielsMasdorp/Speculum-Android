64d300cf0c01f047508925b0cc511e40e5118c7d
NielsMasdorp <nielsmasdorp@gmail.com>
2015-12-30 21:28:19 +0100

    Observable.just() instead of Observable.create()

--- app/src/main/java/com/nielsmasdorp/speculum/services/GoogleCalendarService.java
+++ app/src/main/java/com/nielsmasdorp/speculum/services/GoogleCalendarService.java
@@ -33,67 +33,60 @@ public class GoogleCalendarService {
     }
 
     public Observable<String> getLatestCalendarEvent() {
-        return Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                if (mContext.get() != null) {
-                    String details, title;
-                    Cursor cursor;
-                    ContentResolver contentResolver = mContext.get().getContentResolver();
-                    final String[] colsToQuery = new String[]{
-                            CalendarContract.EventsEntity.TITLE,
-                            CalendarContract.EventsEntity.DTSTART,
-                            CalendarContract.EventsEntity.DTEND,
-                            CalendarContract.EventsEntity.EVENT_LOCATION};
+        if (mContext.get() != null) {
+            String details, title;
+            Cursor cursor;
+            ContentResolver contentResolver = mContext.get().getContentResolver();
+            final String[] colsToQuery = new String[]{
+                    CalendarContract.EventsEntity.TITLE,
+                    CalendarContract.EventsEntity.DTSTART,
+                    CalendarContract.EventsEntity.DTEND,
+                    CalendarContract.EventsEntity.EVENT_LOCATION};
 
-                    Calendar now = Calendar.getInstance();
-                    SimpleDateFormat startFormat = new SimpleDateFormat(Constants.SIMPLEDATEFORMAT_DDMMYY, Locale.getDefault());
-                    String dateString = startFormat.format(now.getTime());
-                    long start = now.getTimeInMillis();
+            Calendar now = Calendar.getInstance();
+            SimpleDateFormat startFormat = new SimpleDateFormat(Constants.SIMPLEDATEFORMAT_DDMMYY, Locale.getDefault());
+            String dateString = startFormat.format(now.getTime());
+            long start = now.getTimeInMillis();
 
-                    SimpleDateFormat endFormat = new SimpleDateFormat(Constants.SIMPLEDATEFORMAT_HHMMSSDDMMYY, Locale.getDefault());
-                    Calendar endOfDay = Calendar.getInstance();
-                    Date endOfDayDate;
-                    try {
-                        endOfDayDate = endFormat.parse(Constants.END_OF_DAY_TIME + dateString);
-                        endOfDay.setTime(endOfDayDate);
-                    } catch (ParseException e) {
-                        subscriber.onError(e);
-                        Log.e(GoogleCalendarService.class.getSimpleName(), e.toString());
-                    }
-
-                    cursor = contentResolver.query(CalendarContract.Events.CONTENT_URI, colsToQuery,
-                            Constants.CALENDAR_QUERY_FIRST + start + Constants.CALENDAR_QUERY_SECOND + endOfDay.getTimeInMillis() + Constants.CALENDAR_QUERY_THIRD,
-                            null, Constants.CALENDAR_QUERY_FOURTH);
-
-                    if (cursor != null) {
-                        if (cursor.getCount() > 0) {
-                            cursor.moveToFirst();
-                            title = cursor.getString(0);
-                            Calendar startTime = Calendar.getInstance();
-                            startTime.setTimeInMillis(cursor.getLong(1));
-                            Calendar endTime = Calendar.getInstance();
-                            endTime.setTimeInMillis(cursor.getLong(2));
-                            DateFormat formatter = SimpleDateFormat.getTimeInstance(DateFormat.SHORT);
-                            details = formatter.format(startTime.getTime()) + " - " + formatter.format(endTime.getTime());
-                            if (!TextUtils.isEmpty(cursor.getString(3))) {
-                                details += " ~ " + cursor.getString(3);
-                            }
-                            cursor.close();
-                            subscriber.onNext(title + ", " + details);
-                            subscriber.onCompleted();
-                        } else {
-                            cursor.close();
-                            subscriber.onNext(mContext.get().getString(R.string.no_events_today));
-                            subscriber.onCompleted();
-                        }
-                    } else {
-                        subscriber.onError(new RuntimeException(mContext.get().getString(R.string.no_events_error)));
-                    }
-                } else {
-                    subscriber.onError(new RuntimeException("View has been garbage collected."));
-                }
+            SimpleDateFormat endFormat = new SimpleDateFormat(Constants.SIMPLEDATEFORMAT_HHMMSSDDMMYY, Locale.getDefault());
+            Calendar endOfDay = Calendar.getInstance();
+            Date endOfDayDate;
+            try {
+                endOfDayDate = endFormat.parse(Constants.END_OF_DAY_TIME + dateString);
+                endOfDay.setTime(endOfDayDate);
+            } catch (ParseException e) {
+                Log.e(GoogleCalendarService.class.getSimpleName(), e.toString());
+                throw new RuntimeException(String.format("ParseException occured: %s", e.getLocalizedMessage()));
             }
-        });
+
+            cursor = contentResolver.query(CalendarContract.Events.CONTENT_URI, colsToQuery,
+                    Constants.CALENDAR_QUERY_FIRST + start + Constants.CALENDAR_QUERY_SECOND + endOfDay.getTimeInMillis() + Constants.CALENDAR_QUERY_THIRD,
+                    null, Constants.CALENDAR_QUERY_FOURTH);
+
+            if (cursor != null) {
+                if (cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+                    title = cursor.getString(0);
+                    Calendar startTime = Calendar.getInstance();
+                    startTime.setTimeInMillis(cursor.getLong(1));
+                    Calendar endTime = Calendar.getInstance();
+                    endTime.setTimeInMillis(cursor.getLong(2));
+                    DateFormat formatter = SimpleDateFormat.getTimeInstance(DateFormat.SHORT);
+                    details = formatter.format(startTime.getTime()) + " - " + formatter.format(endTime.getTime());
+                    if (!TextUtils.isEmpty(cursor.getString(3))) {
+                        details += " ~ " + cursor.getString(3);
+                    }
+                    cursor.close();
+                    return Observable.just(title + ", " + details);
+                } else {
+                    cursor.close();
+                    return Observable.just(mContext.get().getString(R.string.no_events_today));
+                }
+            } else {
+                throw new RuntimeException(mContext.get().getString(R.string.no_events_error));
+            }
+        } else {
+            throw new RuntimeException("View has been garbage collected.");
+        }
     }
 }
