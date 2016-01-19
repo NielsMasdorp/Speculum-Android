56b313d3d67d7e2d91a964aa5a0fdd06bdfdca0f
Gijs de Wit <gijs.de.wit94@gmail.com>
2016-01-19 15:05:40 +0100

    Layout fixed, views binded in MainActivity

--- app/src/main/java/com/nielsmasdorp/speculum/views/MainActivity.java
+++ app/src/main/java/com/nielsmasdorp/speculum/views/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
+import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,53 +36,111 @@ public class MainActivity extends AppCompatActivity implements IMainView, View.O
     @Bind(R.id.main_content)
     ScrollView mMainContent;
 
-    @Bind(R.id.tv_weather_title)
-    TextView mWeatherTitle;
 
-    @Bind(R.id.tv_weather_condition)
-    TextView mWeatherCondition;
+    //Time and date
 
-    @Bind(R.id.tv_weather_astronomy)
-    TextView mWeatherAstronomy;
+    @Bind(R.id.tc_time)
+    TextClock mTime;
 
-    @Bind(R.id.tv_weather_atmosphere)
-    TextView mWeatherAtmosphere;
+    @Bind(R.id.tc_date)
+    TextClock mDate;
 
-    @Bind(R.id.tv_weather_wind)
-    TextView mWeatherWind;
 
-    @Bind(R.id.tv_forecast_day_one_date)
+    //Weather current conditions
+
+    @Bind(R.id.iv_current_weather)
+    ImageView mWeatherCondition;
+
+    @Bind(R.id.tv_current_temp)
+    TextView mWeatherTemp;
+
+
+    //Weather forecast 4 days
+
+    @Bind(R.id.iv_forecast_weather1)
+    ImageView mDayOneCondition;
+
+    @Bind(R.id.tv_forecast_temp1)
+    TextView mDayOneTemp;
+
+    @Bind(R.id.tv_forecast_date1)
     TextView mDayOneDate;
 
-    @Bind(R.id.tv_forecast_day_one_condition)
-    TextView mDayOneCondition;
 
-    @Bind(R.id.tv_forecast_day_two_date)
+    @Bind(R.id.iv_forecast_weather2)
+    ImageView mDayTwoCondition;
+
+    @Bind(R.id.tv_forecast_temp2)
+    TextView mDayTwoTemp;
+
+    @Bind(R.id.tv_forecast_date2)
     TextView mDayTwoDate;
 
-    @Bind(R.id.tv_forecast_day_two_condition)
-    TextView mDayTwoCondition;
 
-    @Bind(R.id.tv_forecast_day_three_date)
+    @Bind(R.id.iv_forecast_weather3)
+    ImageView mDayThreeCondition;
+
+    @Bind(R.id.tv_forecast_temp3)
+    TextView mDayThreeTemp;
+
+    @Bind(R.id.tv_forecast_date3)
     TextView mDayThreeDate;
 
-    @Bind(R.id.tv_forecast_day_three_condition)
-    TextView mDayThreeCondition;
 
-    @Bind(R.id.tv_forecast_day_four_date)
+    @Bind(R.id.iv_forecast_weather4)
+    ImageView mDayFourCondition;
+
+    @Bind(R.id.tv_forecast_temp4)
+    TextView mDayFourTemp;
+
+    @Bind(R.id.tv_forecast_date4)
     TextView mDayFourDate;
 
-    @Bind(R.id.tv_forecast_day_four_condition)
-    TextView mDayFourCondition;
 
-    @Bind(R.id.tv_next_event)
-    TextView mNextEvent;
+    //Weather stats (wind speed+temp, humidity, pressure)
 
-    @Bind(R.id.tv_stock_info)
-    TextView mStockInfo;
+    @Bind(R.id.tv_stats_wind)
+    TextView mWeatherWind;
 
-    @Bind(R.id.tv_reddit_post)
-    TextView mRedditPost;
+    @Bind(R.id.tv_stats_humidity)
+    TextView mWeatherHumidity;
+
+    @Bind(R.id.tv_stats_pressure)
+    TextView mWeatherPressure;
+
+
+    //Sunrise/Sunset times
+
+    @Bind(R.id.tv_sunrise_time)
+    TextView mSunriseTime;
+
+    @Bind(R.id.tv_sunset_time)
+    TextView mSunsetTime;
+
+
+    //Calendar event
+
+    @Bind(R.id.tv_calendar_event)
+    TextView mCalendarEvent;
+
+
+    //reddit post
+
+    @Bind(R.id.tv_reddit_post_title)
+    TextView mRedditPostTitle;
+
+    @Bind(R.id.tv_reddit_post_votes)
+    TextView mRedditPostVotes;
+
+
+    //Stocks (name, value)
+
+//    @Bind(R.id.tv_stock_name)
+//    TextView mStockName;
+//
+//    @Bind(R.id.tv_stock_value)
+//    TextView mStockValue;
+
 
     @Bind(R.id.pb_loading_spinner)
     ProgressBar mProgressLoading;
@@ -134,21 +194,17 @@ public class MainActivity extends AppCompatActivity implements IMainView, View.O
 
         //this.mCurrentWeatherIcon.setImageResource(getResources().getIdentifier(weather.getStatusCode(), "drawable", getPackageName()));
 
-        this.mWeatherTitle.setText(weather.getTitle());
+        this.mWeatherTemp.setText(weather.getTemperature() + "º" + temperature);
 
-        this.mWeatherCondition.setText(weather.getTemperature() + "º" + temperature + ", " +
-                weather.getCondition());
 
         if (mConfiguration.isAtmosphere()) {
-            this.mWeatherAtmosphere.setText(getString(R.string.humidity) + " : " + weather.getHumidity() + "%, " +
-                    getString(R.string.pressure) + ": " +
-                    weather.getPressure() + pressure + ", " + getString(R.string.visibility) + ": " +
-                    weather.getVisibility() + distance);
+            this.mWeatherPressure.setText(getString(R.string.pressure) + ": " + weather.getPressure() + pressure);
+            this.mWeatherHumidity.setText(getString(R.string.humidity) + ": " + weather.getHumidity() + "%");
         }
+
         if (mConfiguration.isSun()) {
-            this.mWeatherAstronomy.setText(getString(R.string.sunrise) + ": " + weather.getSunrise() +
-                    ", " + getString(R.string.sunset) + ": " +
-                    weather.getSunset());
+            this.mSunriseTime.setText(weather.getSunrise());
+            this.mSunsetTime.setText(weather.getSunset());
         }
 
         if (mConfiguration.isWind()) {
@@ -159,17 +215,20 @@ public class MainActivity extends AppCompatActivity implements IMainView, View.O
 
             List<Forecast> forecast = weather.getForecast();
 
-            this.mDayOneDate.setText(forecast.get(0).getDate());
-            this.mDayOneCondition.setText(forecast.get(0).getText() + " " + forecast.get(0).getLow() + "/" + forecast.get(0).getHigh() + "º" + temperature);
+            this.mDayOneDate.setText(forecast.get(0).getDate().substring(0, forecast.get(0).getDate().length() - 5));
+            this.mDayOneTemp.setText(forecast.get(0).getLow() + "/" + forecast.get(0).getHigh() + "º" + temperature);
             //this.mDayOneIcon.setImageResource(getResources().getIdentifier(forecast.get(0).getCode(), "drawable", getPackageName()));
-            this.mDayTwoDate.setText(forecast.get(1).getDate());
-            this.mDayTwoCondition.setText(forecast.get(1).getText() + " " + forecast.get(1).getLow() + "/" + forecast.get(1).getHigh() + "º" + temperature);
+
+            this.mDayTwoDate.setText(forecast.get(1).getDate().substring(0, forecast.get(1).getDate().length() - 5));
+            this.mDayTwoTemp.setText(forecast.get(1).getLow() + "/" + forecast.get(1).getHigh() + "º" + temperature);
             //this.mDayTwoIcon.setImageResource(getResources().getIdentifier(forecast.get(1).getCode(), "drawable", getPackageName()));
-            this.mDayThreeDate.setText(forecast.get(2).getDate());
-            this.mDayThreeCondition.setText(forecast.get(2).getText() + " " + forecast.get(2).getLow() + "/" + forecast.get(2).getHigh() + "º" + temperature);
+
+            this.mDayThreeDate.setText(forecast.get(2).getDate().substring(0, forecast.get(2).getDate().length() - 5));
+            this.mDayThreeTemp.setText(forecast.get(2).getLow() + "/" + forecast.get(2).getHigh() + "º" + temperature);
             //this.mDayThreeIcon.setImageResource(getResources().getIdentifier(forecast.get(2).getCode(), "drawable", getPackageName()));
-            this.mDayFourDate.setText(forecast.get(3).getDate());
-            this.mDayFourCondition.setText(forecast.get(3).getText() + " " + forecast.get(3).getLow() + "/" + forecast.get(3).getHigh() + "º" + temperature);
+
+            this.mDayFourDate.setText(forecast.get(3).getDate().substring(0, forecast.get(3).getDate().length()- 5));
+            this.mDayFourTemp.setText(forecast.get(3).getLow() + "/" + forecast.get(3).getHigh() + "º" + temperature);
             //this.mDayFourIcon.setImageResource(getResources().getIdentifier(forecast.get(3).getCode(), "drawable", getPackageName()));
         }
 
@@ -179,23 +238,23 @@ public class MainActivity extends AppCompatActivity implements IMainView, View.O
     @Override
     public void displayTopRedditPost(RedditPost redditPost) {
 
-        mRedditPost.setText(redditPost.getTitle());
+        mRedditPostTitle.setText(redditPost.getTitle());
         hideProgressbar();
     }
 
     @Override
     public void displayLatestCalendarEvent(String event) {
 
-        this.mNextEvent.setText(getString(R.string.next_event) + ": " + event);
+        this.mCalendarEvent.setText(getString(R.string.next_event) + ": " + event);
         hideProgressbar();
     }
 
     @Override
     public void displayStockInformation(StockInformation stockInformation) {
 
-        this.mStockInfo.setText(getString(R.string.stock_info) + " " + stockInformation.getName()
-                + ": " + stockInformation.getChange() + "%");
-        hideProgressbar();
+//        this.mStockName.setText(getString(R.string.stock_info) + " " + stockInformation.getName()
+//                + ": " + stockInformation.getChange() + "%");
+//        hideProgressbar();
     }
 
     @Override
